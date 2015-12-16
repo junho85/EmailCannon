4b29c5fba8d32b3a32f17cf91761cdc2c1a18d88
june.kim <junho85@gmail.com>
2015-12-16 21:39:06 +0900

    #3 smtp.naver.com 587

--- src/main/java/kr/pe/junho85/service/impl/SendServiceNaver2Impl.java
+++ src/main/java/kr/pe/junho85/service/impl/SendServiceNaver2Impl.java
@@ -3,6 +3,7 @@ package kr.pe.junho85.service.impl;
 import kr.pe.junho85.service.SendService;
 import kr.pe.junho85.util.SampleMailMessageFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.propertyeditors.StringArrayPropertyEditor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
@@ -10,7 +11,7 @@ import org.springframework.stereotype.Service;
 import java.util.Properties;
 
 @Service
-public class SendServiceNaverImpl implements SendService {
+public class SendServiceNaver2Impl implements SendService {
 
     @Value("${smtp.naver.host}")
     private String host;
@@ -23,21 +24,12 @@ public class SendServiceNaverImpl implements SendService {
 
     @Override
     public void send() {
-        int port = 465;
-
-        String from = "jworld2000@naver.com";
-        String to = "junho85@daum.net";
-        String subject = "test naver mail subject";
-        String text = "test naver mail";
+        int port = 587;
 
         Properties properties = new Properties();
         properties.put("mail.smtp.auth", true);
         properties.put("mail.smtp.host", host);
         properties.put("mail.smtp.port", port);
-        properties.put("mail.transport.protocol", "smtps");
-        properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-        properties.put("mail.smtps.quitwait", false);
-        properties.put("mail.smtp.socketFactory.port", port);
         properties.put("mail.smtp.starttls.enable", true);
 
         JavaMailSenderImpl sender = new JavaMailSenderImpl();
---
+++ src/main/java/kr/pe/junho85/util/SampleMailMessageFactory.java
@@ -0,0 +1,25 @@
+package kr.pe.junho85.util;
+
+import org.springframework.mail.SimpleMailMessage;
+
+public class SampleMailMessageFactory {
+    public SimpleMailMessage create() {
+        String from = "jworld2000@naver.com";
+        String to = "junho85@daum.net";
+        String subject = "test naver mail subject2";
+        String text = "test naver mail2";
+
+        SimpleMailMessage templateMessage;
+
+        templateMessage = new SimpleMailMessage();
+
+        templateMessage.setSubject(subject);
+        templateMessage.setFrom(from);
+        templateMessage.setTo(to);
+        templateMessage.setText(text);
+
+        SimpleMailMessage msg = new SimpleMailMessage(templateMessage);
+
+        return msg;
+    }
+}
