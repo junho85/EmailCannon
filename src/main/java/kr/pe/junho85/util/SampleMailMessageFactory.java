e95f7ecbe3521f298f69695c79c22a5878e55afc
june.kim <junho85@gmail.com>
2015-12-18 22:09:51 +0900

    https://github.com/junho85/EmailCannon/issues/4 gmail

--- src/main/java/kr/pe/junho85/util/SampleMailMessageFactory.java
+++ src/main/java/kr/pe/junho85/util/SampleMailMessageFactory.java
@@ -3,9 +3,7 @@ package kr.pe.junho85.util;
 import org.springframework.mail.SimpleMailMessage;
 
 public class SampleMailMessageFactory {
-    public SimpleMailMessage create() {
-        String from = "jworld2000@naver.com";
-        String to = "junho85@daum.net";
+    public SimpleMailMessage create(String from, String to) {
         String subject = "test naver mail subject2";
         String text = "test naver mail2";
 
@@ -21,5 +19,12 @@ public class SampleMailMessageFactory {
         SimpleMailMessage msg = new SimpleMailMessage(templateMessage);
 
         return msg;
+    }
+
+    public SimpleMailMessage create() {
+        String from = "jworld2000@naver.com";
+        String to = "junho85@daum.net";
+
+        return create(from, to);
     }
 }
